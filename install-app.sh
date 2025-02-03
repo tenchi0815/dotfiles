f387c35db8898ac78ffc748ce6c6efeb7d13fc20
tenchi0815 <tenchi.suematsu.p1@dc.tohoku.ac.jp>
2025-02-03 13:04:55 +0900

    Improve
    - Make it possible to add PATH to *shrc
    - Add some softwares to install
    - Add support for detecting os-archi when installing Neovim
    - Fix indents
    - Make dry-run detection more simple

--- install-app.sh
+++ install-app.sh
@@ -1,10 +1,17 @@
 #!/bin/bash -eu
 
-cd "$HOME"
-
 CMDNAME="$(basename "$0")"
+DIRNAME="$(cd "$(dirname "$0")"; pwd)"
+shells="bash zsh fish"
+readonly CMDNAME DIRNAME shells
+essentials=(git curl less jq make unzip xsel)
+DRYRUN=
+prefix=
+update_config=2
+
+cd "$HOME"
 print_usage() {
-  cat - << EOF
+    cat - << EOF
 usage: ${CMDNAME} [OPTIONS]
     -C, --dry-run
         Executes dry run mode; don't actually do anything, just show what will be done.
@@ -30,92 +37,142 @@ msg_done() {
 }
 
 run() {
-  if "${DRYRUN}" ; then
-    echo -e "${prefix}$(quote_each_args "$@")"
-  else
-    "$@"
-    echo 'done.'
-  fi
+    if [[ -n "${DRYRUN}" ]]; then
+        echo -e "${DRYRUN}$(quote_each_args "$@")"
+    else
+        "$@"
+        echo 'done.'
+    fi
 }
 
 quote_each_args() {
-  for i in $(seq 1 $#); do
-    if [[ $i -lt $# ]]; then
-      printf '%q ' "${!i}"
-    else
-      printf '%q' "${!i}"
-    fi
-  done
+    for i in $(seq 1 $#); do
+        if [[ $i -lt $# ]]; then
+            printf '%q ' "${!i}"
+        else
+            printf '%q' "${!i}"
+        fi
+    done
 }
 
 ask() {
-  while true; do
-    read -p "$1 ([y]/n) " -r
-    REPLY=${REPLY:-"y"}
-    if [[ $REPLY =~ ^[Yy]$ ]]; then
-      return 0
-    elif [[ $REPLY =~ ^[Nn]$ ]]; then
-      return 1
-    fi
-  done
+    while true; do
+        read -p "$1 ([y]/n) " -r
+        REPLY=${REPLY:-"y"}
+        if [[ $REPLY =~ ^[Yy]$ ]]; then
+            return 0
+        elif [[ $REPLY =~ ^[Nn]$ ]]; then
+            return 1
+        fi
+    done
 }
 
 download_nvim() {
-local os="$1"
-local archi="$2"
-curl -LO "https://github.com/neovim/neovim/releases/download/v0.10.4/nvim-$os-$archi.tar.gz"
+    local os archi
+    os="$1"
+    archi="$2"
+    curl -LO "https://github.com/neovim/neovim/releases/download/v0.10.4/nvim-$os-$archi.tar.gz"
 }
 
-DRYRUN=false
-prefix=
-
 while [[ $# -gt 0 ]]; do
-  case $1 in
-      -C|--dry-run)
-          DRYRUN=true
-          prefix="[DRY-RUN] "
-          shift # past value
-          ;;
-      -h|--help)
-          print_usage
-          exit 0
-          ;;
-      -*)
-          print_usage
-          errExit "Unknown option: $1"
-          ;;
-      *)
-          print_usage
-          errExit "Unknown argument: $1"
-          ;;
-  esac
+    case $1 in
+        -C|--dry-run)
+            DRYRUN="[DRY-RUN] "
+            shift # past value
+            ;;
+        -h|--help)
+            print_usage
+            exit 0
+            ;;
+        -*)
+            print_usage
+            errExit "Unknown option: $1"
+            ;;
+        *)
+            print_usage
+            errExit "Unknown argument: $1"
+            ;;
+    esac
 done
 
-archi=$(uname -sm)
+append_line() {
+    local update line file pat lines
+    update="$1"
+    line="$2"
+    file="$3"
+    pat="${4:-}"
+    lines=""
 
-# Detect the package manager
+    echo "Update $file:"
+    echo "  - $line"
+    if [ -f "$file" ]; then
+        if [ $# -lt 4 ]; then
+            lines=$(\grep -nF "$line" "$file")
+        else
+            lines=$(\grep -nF "$pat" "$file")
+        fi
+    fi
+
+    if [ -n "$lines" ]; then
+        echo "    - Already exists:"
+        sed 's/^/        Line /' <<< "$lines"
+
+        update=0
+        if ! \grep -qv "^[0-9]*:[[:space:]]*#" <<< "$lines" ; then
+            echo "    - But they all seem to be commented"
+            ask  "    - Continue modifying $file?"
+            update=$?
+        fi
+    fi
+
+    set -e
+    if [ "$update" -eq 1 ]; then
+        [ -f "$file" ] && echo >> "$file"
+        echo "$line" >> "$file"
+        echo "    + Added"
+    else
+        echo "    ~ Skipped"
+    fi
+
+    echo
+    set +e
+}
+
+append_config () {
+    for shell in $shells; do
+        [[ "$shell" = fish ]] && continue
+        if [ "$shell" = zsh ]; then
+            dest=${HOME}/.zshrc.local
+            [ ! -e "$dest" ] && dest=${ZDOTDIR:-$HOME}/.zshrc
+        else
+            dest=~/.bashrc
+            append_line $update_config "[ -f ${prefix}.${shell} ] && source ${prefix}.${shell}" "$dest" "${prefix}.${shell}"
+        fi
+    done
+}
+
+# Detect the package manager and install essential packages.
 if has apt; then
-  echo "Detected apt package manager"
-  pkg_mgr='apt'
+    echo "Detected apt package manager"
+    pkg_mgr='apt'
+    echo '>>> Essentials'
+    run sudo "${pkg_mgr}" update
+    run sudo "${pkg_mgr}" install -y "${essentials[@]}" shellcheck
 elif has dnf; then
-  echo "Detected dnf package manager"
-  pkg_mgr='dnf'
+    echo "Detected dnf package manager"
+    pkg_mgr='dnf'
+    echo '>>> Essentials'
+    run sudo "${pkg_mgr}" update
+    run sudo "${pkg_mgr}" install -y "${essentials[@]}" ShellCheck
 elif has yum; then
-  echo "Detected yum package manager"
-  pkg_mgr='yum'
+    echo "Detected yum package manager"
+    pkg_mgr='yum'
+    echo '>>> Essentials'
+    run sudo "${pkg_mgr}" update
+    run sudo "${pkg_mgr}" install -y "${essentials[@]}" ShellCheck
 else
-  errExit "No compatible package manager found."
+    errExit "No compatible package manager found."
 fi
-echo
-
-# Update repogitories
-echo '>>> Update package repo'
-run sudo "${pkg_mgr}" update
-echo
-
-# Essentials
-echo '>>> Essentials'
-run sudo "${pkg_mgr}" install -y git curl less jq make
 echo
 
 # zsh
@@ -158,43 +215,77 @@ echo
 
 # Neovim
 echo '>>> Neovim'
-if has nvim; then
-    msg_already "Neovim"
-else
-    if ask "Do you want to install Neovim?"; then
-        msg_inprogress "Neovim"
-        binary_available=1
-        binary_error=""
-        case "$archi" in
-          Darwin\ arm64)      run download_nvim "macos" "arm64"  ;;
-          Darwin\ x86_64)     run download_nvim "macos" "arm64"  ;;
-          Linux\ armv5*)      binary_error=1   ;;
-          Linux\ armv6*)      binary_error=1   ;;
-          Linux\ armv7*)      binary_error=1   ;;
-          Linux\ armv8*)      run download_nvim "linux" "arm64"   ;;
-          Linux\ aarch64*)    run download_nvim "macos" "x86_64"  ;;
-          Linux\ *64)         run download_nvim "macos" "x86_64"   ;;
-          CYGWIN*\ *64)       binary_error=1 binary_available=0    ;;
-          MINGW*\ *64)        binary_error=1 binary_available=0    ;;
-          MSYS*\ *64)         binary_error=1 binary_available=0    ;;
-          Windows*\ *64)      binary_error=1 binary_available=0    ;;
-          *)                  binary_error=1   ;;
-        esac
-        if [ -n "$binary_error" ]; then
-            if [ $binary_available -eq 0 ]; then
-                echo "Prebuilt binary for $archi is available. Install manually from https://github.com/neovim/neovim/releases"
-            else
-                echo "No prebuilt binary for $archi ..."
-                echo "Neovim may not suport $archi platform."
-            fi
+if ask "Do you want to install Neovim?"; then
+    msg_inprogress "Neovim"
+    binary_available=1
+    binary_error=""
+    uname="$(uname -sm)"
+    case "$uname" in
+        Darwin\ arm64)      os="macos" archi="arm64"  ;;
+        Darwin\ x86_64)     os="macos" archi="arm64"  ;;
+        Linux\ armv5*)      binary_error=1   ;;
+        Linux\ armv6*)      binary_error=1   ;;
+        Linux\ armv7*)      binary_error=1   ;;
+        Linux\ armv8*)      os="linux" archi="arm64"   ;;
+        Linux\ aarch64*)    os="linux" archi="x86_64"  ;;
+        Linux\ *64)         os="linux" archi="x86_64"   ;;
+        CYGWIN*\ *64)       binary_error=1 binary_available=0    ;;
+        MINGW*\ *64)        binary_error=1 binary_available=0    ;;
+        MSYS*\ *64)         binary_error=1 binary_available=0    ;;
+        Windows*\ *64)      binary_error=1 binary_available=0    ;;
+        *)                  binary_error=1   ;;
+    esac
+    if [ -n "$binary_error" ]; then
+        if [ $binary_available -eq 0 ]; then
+            echo "Prebuilt binary for $uname is available. Install manually from https://github.com/neovim/neovim/releases"
         else
+            echo "No prebuilt binary for $uname ..."
+            echo "Neovim may not suport $uname platform."
+        fi
+    else
+        echo "Downloading nvim ..."
+        nvim_base="nvim-${os}-${archi}"
+        if [ -x /opt/"$nvim_base"/bin/nvim ]; then
+            echo "  - Already exists"
+        else
+            run curl -LO "https://github.com/neovim/neovim/releases/download/v0.10.4/${nvim_base}.tar.gz"
             run sudo rm -rf /opt/nvim
             run sudo tar -C /opt -xzf nvim-"${os}"-"${archi}".tar.gz
+        fi
+        #echo 'export PATH="$PATH:/opt/nvim-linux64/bin"' >> ~/.bashrc
+        echo
 
-            #echo 'export PATH="$PATH:/opt/nvim-linux64/bin"' >> ~/.bashrc
-            msg_done "Neovim"
+        if ask "Do you want to update your shell configuration files?"; then
+            update_config=1
+            for shell in $shells; do
+                [[ "$shell" = fish ]] && continue
+                prefix="${DIRNAME}/nvim/.nvim"
+                src=${prefix}.${shell}
+                echo -n "Generate $src ... "
+
+                cat > "$src" << EOF
+# Setup Neovim
+# -------------------------------------------------------------------------------------------------------
+PATH="\${PATH:+\${PATH}:}/opt/$nvim_base/bin"
+export EDITOR="nvim"
+alias vi='nvim'
+alias vim='nvim'
+alias view='nvim -R'
+
+EOF
+
+                echo "OK"
+                if [ "$shell" = 'zsh' ]; then
+                    dest=${HOME}/.zshrc.local
+                    [ ! -e "$dest" ] && dest=${ZDOTDIR:-$HOME}/.zshrc
+                else
+                    dest=~/.bash_profile # if .zshrc.local
+                fi
+                append_line $update_config "[ -f $src ] && source $src" "$dest" "$src"
+            done
         fi
     fi
+    msg_done "Neovim"
 fi
 echo
 
@@ -210,6 +301,42 @@ else
     run sudo mv zsh-git-escape-magic /usr/share/zsh/functions/Zle/
     run sudo ln -s /usr/share/zsh/functions/Zle/zsh-git-escape-magic/git-escape-magic /usr/share/zsh/functions/Zle/git-escape-magic
     msg_done "git-escape-magic"
+fi
+echo
+
+# vim-plug
+echo '>>> vim-plug'
+if [[ -f "${XDG_DATA_HOME:-$HOME/.local/share}/nvim/site/autoload/plug.vim" ]] ||  [[ -f "${XDG_DATA_HOME:-$HOME/.local/share}/nvim/site/autoload/plug.vim" ]]; then
+    msg_already "vim-plug"
+else
+    if ask "Do you want to install vim-plug?"; then
+        if has nvim; then
+            [[ "$(uname -s)" == "Linux" ]] && run sh -c 'curl -fLoq "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+                https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim' \
+                || echo "Thix script does not suport platform $(uname -s). Install manually."
+                        elif has vim; then
+                            [[ "$(uname -s)" == "Linux" ]] && run curl -fLoq ~/.vim/autoload/plug.vim --create-dirs \
+                                https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim \
+                                || echo "Thix script does not suport platform $(uname -s). Install manually."
+                                                        else
+                                                            echo "Neither nvim nor vim is installed."
+        fi
+    fi
+fi
+echo
+
+# deno
+echo '>>> deno'
+if has deno; then
+    msg_already "deno"
+else
+    if ask "Do you want to install Deno?"; then
+        if [ -x "${DENO_INSTALL:-$HOME/.deno}/bin/deno" ]; then
+            echo "  - Already exists"
+        else
+            run curl -fsSL https://deno.land/install.sh | run sh
+        fi
+    fi
 fi
 echo
 
