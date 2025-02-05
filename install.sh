3ef80dbbf63fcb2bdf80973d35eb6f06376f2106
tenchi0815 <tenchi.suematsu.p1@dc.tohoku.ac.jp>
2025-02-05 14:44:13 +0900

    Fix tmux install path
    - Change name of function

--- install.sh
+++ install.sh
@@ -14,9 +14,10 @@ has() {
     command -v "$1" > /dev/null 2>&1
 }
 
-symlink_dir() {
+symlink() {
     src=$1
     dst=$2
+    ls "$src" > /dev/null
     [ -L "$dst" ] && { echo "Symlink $dst already exists. Overwriting..."; rm -fr "$dst"; }
     [ -e "$dst" ] && { echo "$dst already exists. Are you sure to overwrite it?"; rm -Ir "$dst"; }
     if [ -e "$dst" ]; then echo "Failed to create symlink to $dst"; else ln -sf "$src" "$dst"; fi
@@ -24,11 +25,11 @@ symlink_dir() {
 
 # zsh dotfiles
 echo "Making symbolic links to zsh files..."
-#symlink_dir "${ZDIR}"/.zprofile "${ZDOTDIR:-~}"/.zprofile
-symlink_dir "${ZDIR}"/.zshenv "${ZDOTDIR:-$HOME}"/.zshenv
-symlink_dir "${ZDIR}"/.zshrc "${ZDOTDIR:-$HOME}"/.zshrc
-symlink_dir "${ZDIR}"/.zshrc.local "${ZDOTDIR:-$HOME}"/.zshrc.local
-symlink_dir "${ZDIR}"/.zsh_aliases "${ZDOTDIR:-$HOME}"/.zsh_aliases
+#symlink "${ZDIR}"/.zprofile "${ZDOTDIR:-~}"/.zprofile
+symlink "${ZDIR}"/.zshenv "${ZDOTDIR:-$HOME}"/.zshenv
+symlink "${ZDIR}"/.zshrc "${ZDOTDIR:-$HOME}"/.zshrc
+symlink "${ZDIR}"/.zshrc.local "${ZDOTDIR:-$HOME}"/.zshrc.local
+symlink "${ZDIR}"/.zsh_aliases "${ZDOTDIR:-$HOME}"/.zsh_aliases
 echo 'done.'
 echo
 
@@ -37,12 +38,12 @@ echo "Making symbolic links to vim config files..."
 if has nvim; then
     XDG_CONFIG_HOME=${XDG_CONFIG_HOME:-"${HOME}/.config/"}
     [ -d "${XDG_CONFIG_HOME}" ] || mkdir -p "${XDG_CONFIG_HOME}"
-    symlink_dir "${VIMDIR}/init.vim" "${XDG_CONFIG_HOME}/nvim/init.vim"
-    symlink_dir "${VIMDIR}/nvim.d" "${XDG_CONFIG_HOME}/nvim/nvim.d"
-    symlink_dir "${VIMDIR}/env" "${XDG_CONFIG_HOME}/nvim/env"
+    symlink "${VIMDIR}/init.vim" "${XDG_CONFIG_HOME}/nvim/init.vim"
+    symlink "${VIMDIR}/nvim.d" "${XDG_CONFIG_HOME}/nvim/nvim.d"
+    symlink "${VIMDIR}/env" "${XDG_CONFIG_HOME}/nvim/env"
 elif has vim > /dev/null; then
-    symlink_dir "${VIMDIR}/init.vim" "${HOME}/.vimrc"
-    symlink_dir "${VIMDIR}/nvim.d" "${HOME}/nvim.d"
+    symlink "${VIMDIR}/init.vim" "${HOME}/.vimrc"
+    symlink "${VIMDIR}/nvim.d" "${HOME}/nvim.d"
 else
     echo "Neither nvim nor vim is installed."
     echo "Skipped."
@@ -52,6 +53,6 @@ echo
 
 # TMUX
 echo "Making symbolic links to tmux config files..."
-has nvim && symlink_dir "${TMUXDIR}" "${HOME}/.tmux.conf"
+has tmux && symlink "${TMUXDIR}/.tmux.conf" "${HOME}/tmux.conf"
 echo 'done.'
 echo
